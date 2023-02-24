1b6cea8f3d9bd006051cd89cae6c67e53f658523
Gulebi <glebundiy.2006@mail.ru>
2023-02-25 00:04:20 +0600

    Draggable text added

--- src/script.js
+++ src/script.js
@@ -17,18 +17,24 @@ const addImage = () => {
     const reader = new FileReader();
 
     reader.onload = (event) => {
+        const imageBlock = document.querySelector("#image-block");
         const img = new Image();
 
         img.onload = () => {
             const h = maxHeight;
-            const w = img.width * (maxHeight / img.height);
+            const w = img.width * (h / img.height) > 512 ? 512 : img.width * (h / img.height);
 
             ctx.canvas.width = w;
             ctx.canvas.height = h;
             ctx.drawImage(img, 0, 0, w, h);
+
+            imageBlock.style.width = `${w}px`;
+            imageBlock.style.height = `${h}px`;
         };
 
         img.src = event.target.result;
+
+        imageBlock.querySelector("img").src = event.target.result;
     };
 
     reader.readAsDataURL(file);
